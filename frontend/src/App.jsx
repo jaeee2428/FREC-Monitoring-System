1b89a19a7a01f7cb57f866c66e85786db4d3c4a9
Trishia <tgbasmayor@up.edu.ph>
2026-07-14 17:10:49 +0800

    add dashboard layouts and updated adviser dashboard content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import LoginPage from './pages/LoginPage.jsx'
+import AdviserDashboard from './pages/adviser/dashboard.jsx'
 
 function App() {
-  return <LoginPage />
+  return <AdviserDashboard />
 }
 
 export default App
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import AuthCard from '../components/AuthCard'
+import AdviserDashboard from '../pages/adviser/dashboard'
 
 const accounts = [
     {
