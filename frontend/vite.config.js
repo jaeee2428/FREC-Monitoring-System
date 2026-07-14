359904ecbcf0cc4e314562a65b582181468c5fbe
Jae <jadejaena@gmail.com>
2026-07-14 18:30:36 +0800

    vite.config.js fix merge issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> 9215f9113b1a25db9f02948923b76e930c7d12f7
